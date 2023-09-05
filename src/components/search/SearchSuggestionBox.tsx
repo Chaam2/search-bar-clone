a22f6f7c79774f38b1da3e352729f451f4d468e1
cham2 <dearcham2@gmail.com>
2023-09-06 02:18:15 +0900

    Feat : SearchSuggestionBox 컴포넌트 검색어로 api호출 및 데이터 렌더링

--- src/components/search/SearchSuggestionBox.tsx
+++ src/components/search/SearchSuggestionBox.tsx
@@ -2,26 +2,55 @@
  * SearchSuggestionBox UI rendering
  * state management
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSearchResult } from '../../api/search';
 
-interface ISearchSuggestionBoxProps {
-  keyword: string;
-}
 const SearchSuggestionBox = ({ keyword }: ISearchSuggestionBoxProps) => {
+  const [searchResult, setSearchResult] = useState<ISearchResult[]>([]);
+  useEffect(() => {
+    getSearchResultData();
+  }, [keyword]);
+
+  const getSearchResultData = async () => {
+    const response = await getSearchResult(keyword);
+    const searchResultData = response.data;
+    setSearchResult(searchResultData);
+  };
   if (!keyword) {
     return (
       <div>
-        <div>최근 검색어</div>
-        <div>추천 검색어로 검색해보세요</div>
+        <h3>최근 검색어</h3>
+        <h3>추천 검색어로 검색해보세요</h3>
       </div>
     );
   }
   return (
     <div>
       <div>{keyword}</div>
-      <div>추천 검색어</div>
+      <div>
+        <h3>추천 검색어</h3>
+        {searchResult.length === 0 ? (
+          <span>추천 검색어가 없습니다.</span>
+        ) : (
+          searchResult.map(result => {
+            return (
+              <div key={result.sickCd}>
+                <span>{result.sickNm}</span>
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 };
 
 export default SearchSuggestionBox;
+
+interface ISearchSuggestionBoxProps {
+  keyword: string;
+}
+interface ISearchResult {
+  sickCd: string;
+  sickNm: string;
+}
