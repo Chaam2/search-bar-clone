b9acadaf46fdc1ac21ad2c18c5a02d8fd4290682
cham2 <dearcham2@gmail.com>
2023-09-06 23:34:09 +0900

    Design : SearchSuggestionBox 컴포넌트 UI 스타일링

--- src/components/search/SearchSuggestionBox.tsx
+++ src/components/search/SearchSuggestionBox.tsx
@@ -3,6 +3,8 @@
  * state management
  */
 import React, { useEffect, useState } from 'react';
+import { LuSearch } from 'react-icons/lu';
+import { styled } from 'styled-components';
 import { getSearchResult } from '../../api/search';
 import { TypeSearchResult } from '../../types/TypeSearchResult';
 
@@ -18,30 +20,42 @@ const SearchSuggestionBox = ({ keyword, debouncedKeyword }: TypeSearchSuggestion
   };
   if (!keyword) {
     return (
-      <div>
-        <h3>최근 검색어</h3>
-        <h3>추천 검색어로 검색해보세요</h3>
-      </div>
+      <RecentContainer>
+        <div>
+          <h3>최근 검색어</h3>
+        </div>
+        <div>
+          <h3>추천 검색어로 검색해보세요</h3>
+        </div>
+      </RecentContainer>
     );
   }
   return (
-    <div>
-      <div>{keyword}</div>
+    <SearchSuggestionBoxContainer>
+      <ul>
+        <li>
+          <LuSearch size={20} color={'#aaaaaa'} />
+          {keyword}
+        </li>
+      </ul>
       <div>
         <h3>추천 검색어</h3>
         {searchResult.length === 0 ? (
           <span>추천 검색어가 없습니다.</span>
         ) : (
-          searchResult.slice(0, MAX_RESULT).map(result => {
-            return (
-              <div key={result.sickCd}>
-                <span>{result.sickNm}</span>
-              </div>
-            );
-          })
+          <SuggestionUl>
+            {searchResult.slice(0, MAX_RESULT).map(result => {
+              return (
+                <li key={result.sickCd}>
+                  <LuSearch size={20} color={'#aaaaaa'} />
+                  {result.sickNm}
+                </li>
+              );
+            })}
+          </SuggestionUl>
         )}
       </div>
-    </div>
+    </SearchSuggestionBoxContainer>
   );
 };
 
@@ -53,3 +67,74 @@ type TypeSearchSuggestionBoxProps = {
 };
 
 const MAX_RESULT = 7;
+
+const SuggestionContainerStyle = `
+box-sizing: border-box;
+background-color: #ffffff;
+width: 500px;
+box-shadow: 0px 2px 4px #1e202519;
+border-radius: 24px;
+margin-top: 8px;
+`;
+
+const SectionTitleStyle = `
+font-size: 0.9rem;
+font-weight: 400;
+color: #666666;
+margin:0;
+`;
+const KeywordStyle = `
+cursor: pointer;
+font-weight: 500;
+padding: 14px 20px;
+display:flex;
+align-items:center;
+gap:8px;
+&:hover{
+  background-color: #f4f4f4;
+}
+`;
+
+const SearchSuggestionBoxContainer = styled.div`
+  ${SuggestionContainerStyle}
+  padding:20px 0;
+  h3 {
+    ${SectionTitleStyle}
+    padding:10px 20px;
+  }
+  span {
+    padding: 14px 20px;
+    color: #aaaaaa;
+    font-weight: 600;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      ${KeywordStyle}
+    }
+  }
+`;
+
+const SuggestionUl = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  li {
+    ${KeywordStyle}
+  }
+`;
+
+const RecentContainer = styled.div`
+  ${SuggestionContainerStyle}
+  div {
+    padding: 20px;
+    &:first-child {
+      border-bottom: 1px solid #eaeaea;
+    }
+  }
+  h3 {
+    ${SectionTitleStyle}
+  }
+`;
