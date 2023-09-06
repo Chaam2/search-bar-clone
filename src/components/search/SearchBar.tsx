fab1d0f75a7e2486a008e40c10aa24330fafb84e
cham2 <dearcham2@gmail.com>
2023-09-06 22:17:59 +0900

    Design : SearchBar UI 스타일링

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -5,28 +5,86 @@
 import React, { useState } from 'react';
 import { useDebounce } from '../../hooks/useDebounce';
 import SearchSuggestionBox from './SearchSuggestionBox';
+import { LuSearch, LuX } from 'react-icons/lu';
+import { styled } from 'styled-components';
 
 const SearchBar = () => {
   const [keyword, setKeyword] = useState('');
+  const [isFocused, setIsFocused] = useState(true);
   const debouncedKeyword = useDebounce(keyword);
 
   return (
     <div>
-      <div>
-        <input
+      <SearchBarContainer isFocused={isFocused}>
+        <SearchBarInput
           type="text"
           placeholder="질환명을 입력해 주세요."
           value={keyword}
-          onChange={e => {
-            setKeyword(e.target.value);
-          }}
+          onChange={e => setKeyword(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          autoFocus
         />
-        <button>X</button>
-        <button>검색</button>
-      </div>
-      <SearchSuggestionBox keyword={keyword} debouncedKeyword={debouncedKeyword} />
+        {isFocused && (
+          <CancelButton
+            onClick={() => {
+              setKeyword('');
+            }}
+          >
+            <LuX size={16} />
+          </CancelButton>
+        )}
+        <SearchButton>
+          <LuSearch size={24} color={'#ffffff'} />
+        </SearchButton>
+      </SearchBarContainer>
+      {!isFocused && <SearchSuggestionBox keyword={keyword} debouncedKeyword={debouncedKeyword} />}
     </div>
   );
 };
 
 export default SearchBar;
+
+const SearchBarContainer = styled.div<{ isFocused: boolean }>`
+  border-radius: 42px;
+  border: 2px solid ${props => (props.isFocused ? '#007be9' : '#ffffff')};
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+  width: 500px;
+  padding: 8px;
+  box-shadow: 0px 2px 4px #1e202519;
+  cursor: pointer;
+`;
+
+const SearchBarInput = styled.input`
+  font-size: 1rem;
+  font-weight: 400;
+  flex: 1;
+  border: 0;
+  padding: 16px;
+  outline: none;
+  background-color: transparent;
+`;
+
+const ButtonStyle = `
+background-color:#ffffff;
+border: none;
+margin: 0;
+padding: 0;
+cursor: pointer;
+`;
+
+const SearchButton = styled.button`
+  ${ButtonStyle}
+  background-color: #007be9;
+  width: 48px;
+  height: 48px;
+  border-radius: 100px;
+`;
+
+const CancelButton = styled.button`
+  ${ButtonStyle}
+`;
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -2,19 +2,32 @@
  * searchPage UI rendering
  */
 import React from 'react';
+import { styled } from 'styled-components';
 import SearchBar from './SearchBar';
 
-const SearchPage = () => {
+const Search = () => {
   return (
-    <div>
+    <SearchContainer>
       <h1>
         국내 모든 임상시험 검색하고
         <br />
         온라인으로 참여하기
       </h1>
       <SearchBar />
-    </div>
+    </SearchContainer>
   );
 };
 
-export default SearchPage;
+export default Search;
+
+const SearchContainer = styled.div`
+  background-color: #cae9ff;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    text-align: center;
+    margin-top: 80px;
+  }
+`;
