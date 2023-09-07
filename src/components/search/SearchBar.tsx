7f1d78f1e055e6e6b47d94bb62c596bd147c51d3
cham2 <dearcham2@gmail.com>
2023-09-08 00:37:21 +0900

    Fix : 검색어 추천영역 SearchBar컴포넌트 내부로 이동

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,10 +1,5 @@
-/** interface
- * SearchBar UI rendering
- * state management
- */
 import React, { useEffect, useState } from 'react';
 import { useDebounce } from '../../hooks/useDebounce';
-import SearchSuggestionBox from './SearchSuggestionBox';
 import { LuSearch, LuX } from 'react-icons/lu';
 import { styled } from 'styled-components';
 import { getSearchResult } from '../../api/search';
@@ -22,6 +17,10 @@ const SearchBar = ({ isFocused }: TypeSearchBarProps) => {
   const getSearchResultData = async () => {
     const searchResultData = await getSearchResult(debouncedKeyword);
     setSearchResult(searchResultData);
+  };
+
+  const changeKeyword = (newKeyword: string) => {
+    setKeyword(newKeyword);
   };
 
   return (
@@ -48,12 +47,43 @@ const SearchBar = ({ isFocused }: TypeSearchBarProps) => {
           <LuSearch size={24} color={'#ffffff'} />
         </SearchButton>
       </SearchBarContainer>
-      <SearchSuggestionBox
-        keyword={keyword}
-        setKeyword={setKeyword}
-        isFocused={isFocused}
-        searchResult={searchResult}
-      />
+
+      {keyword ? (
+        <SearchSuggestionBoxContainer isFocused={isFocused}>
+          <ul>
+            <li>
+              <LuSearch size={20} color={'#aaaaaa'} />
+              {keyword}
+            </li>
+          </ul>
+          <div>
+            <h3>추천 검색어</h3>
+            {searchResult.length === 0 ? (
+              <span>추천 검색어가 없습니다.</span>
+            ) : (
+              <SuggestionUl>
+                {searchResult?.slice(0, MAX_RESULT).map(result => {
+                  return (
+                    <li key={result.sickCd} onClick={() => changeKeyword(result.sickNm)}>
+                      <LuSearch size={20} color={'#aaaaaa'} />
+                      {result.sickNm}
+                    </li>
+                  );
+                })}
+              </SuggestionUl>
+            )}
+          </div>
+        </SearchSuggestionBoxContainer>
+      ) : (
+        <RecentContainer isFocused={isFocused}>
+          <div>
+            <h3>최근 검색어</h3>
+          </div>
+          <div>
+            <h3>추천 검색어로 검색해보세요</h3>
+          </div>
+        </RecentContainer>
+      )}
     </>
   );
 };
@@ -108,4 +138,81 @@ const SearchButton = styled.button`
 const CancelButton = styled.button<{ isFocused: boolean }>`
   color: ${props => (props.isFocused ? 'black' : 'white')};
   ${ButtonStyle}
+`;
+
+//suggestionBox
+
+const MAX_RESULT = 7;
+
+const SuggestionContainerStyle = `
+box-sizing: border-box;
+background-color: #ffffff;
+width: 500px;
+box-shadow: 0px 2px 4px #1e202519;
+border-radius: 24px;
+margin-top: 8px;
+`;
+
+const SectionTitleStyle = `
+font-size: 0.9rem;
+font-weight: 400;
+color: #666666;
+margin:0;
+`;
+const KeywordStyle = `
+cursor: pointer;
+font-weight: 500;
+padding: 14px 20px;
+display:flex;
+align-items:center;
+gap:8px;
+&:hover{
+  background-color: #f4f4f4;
+}
+`;
+
+const SearchSuggestionBoxContainer = styled.div<{ isFocused: boolean }>`
+  opacity: ${props => (props.isFocused ? '100' : '0')};
+  ${SuggestionContainerStyle}
+  padding:20px 0;
+  h3 {
+    ${SectionTitleStyle}
+    padding:10px 20px;
+  }
+  span {
+    padding: 14px 20px;
+    color: #aaaaaa;
+    font-weight: 600;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      ${KeywordStyle}
+    }
+  }
+`;
+
+const SuggestionUl = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  li {
+    ${KeywordStyle}
+  }
+`;
+
+const RecentContainer = styled.div<{ isFocused: boolean }>`
+  opacity: ${props => (props.isFocused ? '100' : '0')};
+  ${SuggestionContainerStyle}
+  div {
+    padding: 20px;
+    &:first-child {
+      border-bottom: 1px solid #eaeaea;
+    }
+  }
+  h3 {
+    ${SectionTitleStyle}
+  }
 `;
